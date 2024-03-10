79f565f71f7f600fe4b0f22f7fd90d3148e47aca
Alisson Youssf <youssfbr@gmail.com>
2024-03-10 13:34:48 -0300

    Initial commit

---
+++ src/main/java/com/github/youssfbr/uri2602/Uri2602Application.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.uri2602;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uri2602Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2602Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/youssfbr/uri2602/Uri2602ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.uri2602;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uri2602ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
